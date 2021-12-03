3a127342467b4570ff0c3fbf706816b245f6b554
JuanPabloRomero <jp.romero12@uniandes.edu.co>
2021-12-03 14:06:25 -0500

    Commit parcial 1
    Adjunto parcial 1

--- frontreact/src/pages/Home.js
+++ frontreact/src/pages/Home.js
@@ -1,14 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export const Home = ({ searchKey }) => {
   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/api/products?q=" + searchKey)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [searchKey]);
 
   return (
-    <section id='home'>
-      <div className='home-container'>
+    <section id="home">
+      <div className="home-container">
         <h1>Gallery</h1>
-        <div className='home-card'>
-          <p>Show here product cards</p>
+        <div className="home-card">
+          {products.map((item, index) => {
+            return(
+              <div className = "card">
+              <Card name = {item.name} picture = {item.picture} price = {item.price} isActive = {item.isActive}></Card>
+              </div>
+            )            
+          })}
         </div>
       </div>
     </section>
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var data = require("../assets/data")
 
 /* GET products listing. Please establish connection with getProduct function from controllers/product.js  */
 router.get('/', function (req, res, next) {
-  res.send(
-    'To implement. Please establish connection with getProduct function from controllers/product.js'
-  );
+  
+    let dataEnviada = data.filter((p) => {
+      return p.name.toLowerCase().includes(req.query.q)
+    });
+    
+    res.send(dataEnviada);
 });
 
 module.exports = router;
